1c7fdcc8288f22855d9826b0764f208d96992133
princeprakhar <prakhardeep173@gmail.com>
2024-09-29 14:46:12 +0530

    completed create  a blob object

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,28 @@ import java.io.*;
 import java.nio.file.Files;
 import java.util.zip.InflaterInputStream;
 import java.util.zip.DeflaterOutputStream;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Main {
+    //sha1hex
+    public static String sha1Hex(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] messageDigest = md.digest(input.getBytes());
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : messageDigest) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+//  cat-file
   public static  void catFileHandler(String args) throws IOException
   {
     String hashOfDirAndFilename = args;
@@ -19,6 +39,55 @@ public class Main {
     {
       throw new RuntimeException(e);
     }
+  }
+  // string formaater for path
+    private static String shaToPath(String path)
+    {
+        return String.format(".git/objects/%s/%s", path.substring(0, 2),
+                path.substring(2));
+    }
+
+  // hash-ObjectHandler  || createBlobObject
+  public static void createBlobObject(String []args)throws IOException
+  {
+
+      boolean write = false;
+      String fileName = "";
+      for(String arg : args){
+          if(arg.equals("-w")){
+              write = true;
+              fileName = args[2];
+          }else{
+              fileName = args[2];
+          }
+      }
+      try {
+          long fileSize = Files.size(Paths.get(fileName));
+          byte []fileContents = Files.readAllBytes(Paths.get(fileName));
+          final String fileContentStringFormat = new String (fileContents);
+//          header =   blob <size>\0<content>
+//          content = fileContentStringFormat
+//          combinedData in file = header + content;
+          String header = "Blob" + fileSize +"\0";
+          String combinedData = header + fileContentStringFormat;
+          String SHA_1_hash = sha1Hex(combinedData);
+
+          String blobPath = shaToPath(SHA_1_hash);
+          File blobFile = new File(blobPath);
+          blobFile.getParentFile().mkdirs();
+          DeflaterOutputStream out =
+                  new DeflaterOutputStream(new FileOutputStream(blobFile));
+          out.write("blob".getBytes());
+          out.write(" ".getBytes());
+          out.write(String.valueOf(fileSize).getBytes());
+          out.write(0);
+          out.write(fileContents);
+          out.close();
+          System.out.println(SHA_1_hash);
+
+      }catch (IOException e){
+          throw new RuntimeException(e);
+      }
   }
   public static void main(String[] args) throws IOException {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -45,6 +114,9 @@ public class Main {
        }
        case "cat-file" ->{
          catFileHandler(args[2]);
+       }
+       case "hash-object"->{
+         createBlobObject(args);
        }
        default -> System.out.println("Unknown command: " + command);
      }
